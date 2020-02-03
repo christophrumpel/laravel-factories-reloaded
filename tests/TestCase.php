05b9c87bedc9375ab0f87cf3ea1cdce869511bfa
Christoph Rumpel <c.rumpel@kabsi.at>
2020-02-03 15:53:10 +0100

    Fix wrong file deletion path

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -24,7 +24,7 @@ class TestCase extends \Orchestra\Testbench\TestCase
     {
         parent::tearDown();
 
-        if(file_exists('__DIR__.\'/factories/tmp/GroupFactory.php')) {
+        if (file_exists(__DIR__.'/factories/tmp/GroupFactory.php')) {
             unlink(__DIR__.'/factories/tmp/GroupFactory.php');
         }
     }
