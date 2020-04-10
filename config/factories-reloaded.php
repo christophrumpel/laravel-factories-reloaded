13dbfb1a60ac0c2a29bb25f54369763ad9488291
Christoph Rumpel <c.rumpel@kabsi.at>
2020-04-10 15:14:24 +0200

    Update config comments

--- config/factories-reloaded.php
+++ config/factories-reloaded.php
@@ -1,16 +1,22 @@
 <?php
 
-/*
- * You can place your custom package configuration in here.
- */
 return [
+    /*
+     * Defines where your models are located.
+     * Will be used to find your models while generating new factories.
+     */
     'models_paths' => [
         base_path('app'),
     ],
 
+    /**
+     * Defines where your new factories should be stored.
+     */
     'factories_path' => base_path('tests/Factories'),
 
-    'factories_namespace' => 'Tests\Factories',
-
+    /**
+     * Defines where your Laravel factories are located.
+     * They are used while generating new factories.
+     */
     'vanilla_factories_path' => database_path('factories'),
 ];
