627161fbb5e960b4beaa61e3c1bca57e2b16e7ab
Daniel Bushkanets <d.bushkanets@gmail.com>
2020-06-30 08:37:24 -0700

    fix test to fail if using map

--- tests/FactoryTest.php
+++ tests/FactoryTest.php
@@ -208,7 +208,7 @@ class FactoryTest extends TestCase
     public function it_lets_you_use_a_closure_for_overriding_default_data(): void
     {
         $ingredient = IngredientFactoryUsingClosure::new()->create([
-            'name' => 'Basil',
+            'name' => fn (array $ingredient) => 'Basil',
             'description' => fn (array $ingredient) => "Super delicious {$ingredient['name']}",
         ]);
 
